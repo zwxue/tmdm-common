7e94d377cf22bf9924e6c9044f93d4ea7bd6df5d
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-10-19 07:12:37 +0000

    Bug 16451 : Promote former Default_Viewer role to System_Web role
    git-svn-id: http://talendforge.org/svn/tom/trunk@50165 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/ICoreConstants.java
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/ICoreConstants.java
@@ -42,7 +42,7 @@ public interface ICoreConstants {
 		static{
 			oldRoleToNewRoleMap.put("Default_Admin", ICoreConstants.SYSTEM_ADMIN_ROLE);
 			oldRoleToNewRoleMap.put("Default_User", ICoreConstants.SYSTEM_INTERACTIVE_ROLE);
-			oldRoleToNewRoleMap.put("Default_Viewer", ICoreConstants.SYSTEM_VIEW_ROLE);
+			oldRoleToNewRoleMap.put("Default_Viewer", ICoreConstants.SYSTEM_WEB_ROLE);
 		};
 	}
 
