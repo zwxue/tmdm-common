334f13f987131060db76a4a04923a58262145fdc
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-07 05:48:42 +0000

    add feature  	 0020008: Consistency of process results
    git-svn-id: http://talendforge.org/svn/tom/trunk@63979 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/ITransformerConstants.java
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/ITransformerConstants.java
@@ -2,14 +2,14 @@ package org.talend.mdm.commmon.util.core;
 
 public interface ITransformerConstants {
 	
-	public static final String VARIABLE_DEFAULT = "_DEFAULT_";
+    public static final String VARIABLE_DEFAULT = "_DEFAULT_"; //$NON-NLS-1$
 	
-	public static final String VARIABLE_OUTPUT_OF_BEFORESAVINGTRANFORMER = "output_error_message";
+    public static final String VARIABLE_OUTPUT_OF_BEFORESAVINGTRANFORMER = "output_report"; //$NON-NLS-1$
 	
-	public static final String VARIABLE_OUTPUT_TO_ITEMDISPATCHERSERVICE = "output_to_itemdispatcher";
+	public static final String VARIABLE_OUTPUT_TO_ITEMDISPATCHERSERVICE = "output_to_itemdispatcher"; //$NON-NLS-1$
 	
-	public static final String VARIABLE_OUTPUT_TO_JDBCSERVICE = "output_to_jdbcservice";
+	public static final String VARIABLE_OUTPUT_TO_JDBCSERVICE = "output_to_jdbcservice"; //$NON-NLS-1$
 	
-	public static final String VARIABLE_OUTPUT_FOR_SMARTVIEW = "html";
+	public static final String VARIABLE_OUTPUT_FOR_SMARTVIEW = "html"; //$NON-NLS-1$
 	
 }
