503ca3890b2a3f4201ecffefa83a3c88e9a70bbd
mhirt <mhirt@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-17 09:24:54 +0000

    Subversion admin: release 5.0.1 creation from trunk r74687
    git-svn-id: http://talendforge.org/svn/tom/tags/release-5_0_0@74730 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ trunk/org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/MDMConfiguration.java
@@ -0,0 +1,131 @@
+package org.talend.mdm.commmon.util.core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+/**
+ * Handles the mdm.conf file
+ * @author bgrieder
+ *
+ */
+public final class MDMConfiguration {
+     
+    private static String MDM_CONF = "mdm.conf"; //$NON-NLS-1$
+    
+    private static File file;
+    
+    private static Properties CONFIGURATION = null;
+    
+    private MDMConfiguration() {}
+    
+    public final static Properties getConfiguration(){
+        return getConfiguration(false);
+    }
+    
+    public final static Properties getConfiguration(boolean reload){
+        if (reload) CONFIGURATION = null;
+        if (CONFIGURATION != null) return CONFIGURATION;
+        
+        CONFIGURATION = new Properties();
+        
+        String currentDir = System.getProperty("user.dir"); //$NON-NLS-1$
+        file = new File(currentDir, MDM_CONF);
+        
+        // try the current dir
+        if (!file.exists()) {
+            // if not found, try appending "bin"
+            System.out.println("MDM Configuration: unable to find the configuration in '" + file.getAbsolutePath() + "'.");
+            file = new File(currentDir, "bin/" + MDM_CONF);
+            System.out.println("MDM Configuration: trying in '" + file.getAbsolutePath() + "'.");
+        }
+        
+        if (file.exists()) {
+            System.out.println("MDM Configuration: found in '" + file.getAbsolutePath() + "'.");
+            try {
+                CONFIGURATION.load(new FileInputStream(file));
+            }
+            catch (Exception e) {
+                System.err.println("MDM Configuration: unable to load the configuration in '"+file.getAbsolutePath()+"' :"+e.getMessage()+". The default configurations will be used."); 
+            }
+        }
+        else
+            System.err.println("MDM Configuration: unable to load the configuration in '"+file.getAbsolutePath() +". The default configurations will be used.");
+        
+        checkupPropertiesForXDBConf();
+        
+        return CONFIGURATION;
+        
+    }
+    
+    /**
+     * check up xdb config properties to add default value if it is unavailable
+     */
+    @SuppressWarnings("nls")
+    private static void checkupPropertiesForXDBConf()
+    {
+        if (CONFIGURATION == null) return;
+        
+        if (CONFIGURATION.getProperty("xmldb.server.name") == null)
+            CONFIGURATION.setProperty("xmldb.server.name", "localhost");
+        if (CONFIGURATION.getProperty("xmldb.server.port") == null)
+            CONFIGURATION.setProperty("xmldb.server.port", "8180");
+        if (CONFIGURATION.getProperty("xmldb.administrator.username") == null)
+            CONFIGURATION.setProperty("xmldb.administrator.username", "admin");
+        if (CONFIGURATION.getProperty("xmldb.administrator.password") == null)
+            CONFIGURATION.setProperty("xmldb.administrator.password", "1bc29b36f623ba82aaf6724fd3b16718");
+        if (CONFIGURATION.getProperty("xmldb.driver") == null)
+            CONFIGURATION.setProperty("xmldb.driver", "org.exist.xmldb.DatabaseImpl");
+        if (CONFIGURATION.getProperty("xmldb.dbid") == null)
+            CONFIGURATION.setProperty("xmldb.dbid", "exist");
+        if (CONFIGURATION.getProperty("xmldb.dburl") == null)
+            CONFIGURATION.setProperty("xmldb.dburl", "exist/xmlrpc/db");
+        if (CONFIGURATION.getProperty("xmldb.isupurl") == null)
+            CONFIGURATION.setProperty("xmldb.isupurl", "exist/");
+    }
+    /**
+     * save configure file
+     */
+    public static void save(){
+        if(file == null)
+            throw new IllegalStateException();
+        
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            CONFIGURATION.store(out, "MDM configuration file"); //$NON-NLS-1$
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (Exception e2) {
+                }
+            }
+        }
+    }
+    
+    public static EDBType getDBType(){
+        Object dbtype=getConfiguration().get("xmldb.type");
+        if(dbtype!=null && dbtype.toString().equals(EDBType.ORACLE.getName())){
+            return EDBType.ORACLE;
+        }
+        if(dbtype!=null && dbtype.toString().equals(EDBType.BERKELEY.getName())){
+            return EDBType.BERKELEY;
+        }   
+        if(dbtype!=null && dbtype.toString().equals(EDBType.QIZX.getName())){
+            return EDBType.QIZX;
+        } 
+        return EDBType.EXIST;
+    }
+    public static boolean isExistDb() {
+        Object dbtype=getConfiguration().get("xmldb.type");
+        if(dbtype!=null && !dbtype.toString().equals(EDBType.EXIST.getName())) {
+            return false;
+        }
+        return true;
+    }   
+}
