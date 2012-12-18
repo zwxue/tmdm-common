5c6b9ce1e210519dff698f11d7258abe5ebbdf01
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-18 15:57:25 +0000

    TMDM-5138: Use Maven for building org.talend.mdm.core. Class clean up. build.xml updates due to change in directory uses for compilation in org.talend.mdm.core.
    git-svn-id: http://talendforge.org/svn/tom/trunk@95596 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/MDMConfiguration.java
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/MDMConfiguration.java
@@ -21,34 +21,36 @@ import org.apache.log4j.Logger;
 
 /**
  * Handles the mdm.conf file
- * @author bgrieder
  *
+ * @author bgrieder
  */
 public final class MDMConfiguration {
-    
-    private static Logger logger = Logger.getLogger(MDMConfiguration.class);
 
-    private static String MDM_CONF = "mdm.conf"; //$NON-NLS-1$
-    
+    private static final Logger logger = Logger.getLogger(MDMConfiguration.class);
+
+    private static final String MDM_CONF = "mdm.conf"; //$NON-NLS-1$
+
     private static File file;
-    
+
     private static Properties CONFIGURATION = null;
-    
-    private MDMConfiguration() {}
-    
-    public final static Properties getConfiguration(){
+
+    private MDMConfiguration() {
+    }
+
+    public static Properties getConfiguration() {
         return getConfiguration(false);
     }
-    
-    public final static Properties getConfiguration(boolean reload){
-        if (reload) CONFIGURATION = null;
-        if (CONFIGURATION != null) return CONFIGURATION;
-        
+
+    public static Properties getConfiguration(boolean reload) {
+        if (reload) {
+            CONFIGURATION = null;
+        }
+        if (CONFIGURATION != null) {
+            return CONFIGURATION;
+        }
         CONFIGURATION = new Properties();
-        
         String currentDir = System.getProperty("user.dir"); //$NON-NLS-1$
         file = new File(currentDir, MDM_CONF);
-        
         // try the current dir
         if (!file.exists()) {
             // if not found, try appending "bin"
@@ -56,96 +58,99 @@ public final class MDMConfiguration {
             file = new File(currentDir, "bin/" + MDM_CONF);
             logger.info("MDM Configuration: trying in '" + file.getAbsolutePath() + "'.");
         }
-        
         if (file.exists()) {
             logger.info("MDM Configuration: found in '" + file.getAbsolutePath() + "'.");
             try {
                 CONFIGURATION.load(new FileInputStream(file));
+            } catch (Exception e) {
+                logger.warn("MDM Configuration: unable to load the configuration in '" + file.getAbsolutePath() + "' :" + e.getMessage() + ". The default configurations will be used.");
             }
-            catch (Exception e) {
-                logger.warn("MDM Configuration: unable to load the configuration in '"+file.getAbsolutePath()+"' :"+e.getMessage()+". The default configurations will be used.");
-            }
-        }
-        else
+        } else {
             logger.warn("MDM Configuration: unable to load the configuration in '" + file.getAbsolutePath()
                     + ". The default configurations will be used.");
-        
+        }
         checkupPropertiesForXDBConf();
-        
         return CONFIGURATION;
-        
     }
-    
+
     /**
      * check up xdb config properties to add default value if it is unavailable
      */
-    @SuppressWarnings("nls")
-    private static void checkupPropertiesForXDBConf()
-    {
-        if (CONFIGURATION == null) return;
-        
-        if (CONFIGURATION.getProperty("xmldb.server.name") == null)
-            CONFIGURATION.setProperty("xmldb.server.name", "localhost");
-        if (CONFIGURATION.getProperty("xmldb.server.port") == null)
-            CONFIGURATION.setProperty("xmldb.server.port", "8180");
-        if (CONFIGURATION.getProperty("xmldb.administrator.username") == null)
-            CONFIGURATION.setProperty("xmldb.administrator.username", "admin");
-        if (CONFIGURATION.getProperty("xmldb.administrator.password") == null)
-            CONFIGURATION.setProperty("xmldb.administrator.password", "1bc29b36f623ba82aaf6724fd3b16718");
-        if (CONFIGURATION.getProperty("xmldb.driver") == null)
-            CONFIGURATION.setProperty("xmldb.driver", "org.exist.xmldb.DatabaseImpl");
-        if (CONFIGURATION.getProperty("xmldb.dbid") == null)
-            CONFIGURATION.setProperty("xmldb.dbid", "exist");
-        if (CONFIGURATION.getProperty("xmldb.dburl") == null)
-            CONFIGURATION.setProperty("xmldb.dburl", "exist/xmlrpc/db");
-        if (CONFIGURATION.getProperty("xmldb.isupurl") == null)
-            CONFIGURATION.setProperty("xmldb.isupurl", "exist/");
+    private static void checkupPropertiesForXDBConf() {
+        if (CONFIGURATION == null) {
+            return;
+        }
+        if (CONFIGURATION.getProperty("xmldb.server.name") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.server.name", "localhost"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (CONFIGURATION.getProperty("xmldb.server.port") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.server.port", "8180"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (CONFIGURATION.getProperty("xmldb.administrator.username") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.administrator.username", "admin"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (CONFIGURATION.getProperty("xmldb.administrator.password") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.administrator.password", "1bc29b36f623ba82aaf6724fd3b16718"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (CONFIGURATION.getProperty("xmldb.driver") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.driver", "org.exist.xmldb.DatabaseImpl"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (CONFIGURATION.getProperty("xmldb.dbid") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.dbid", "exist"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (CONFIGURATION.getProperty("xmldb.dburl") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.dburl", "exist/xmlrpc/db"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (CONFIGURATION.getProperty("xmldb.isupurl") == null) { //$NON-NLS-1$
+            CONFIGURATION.setProperty("xmldb.isupurl", "exist/"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
     }
+
     /**
      * save configure file
      */
-    public static void save(){
-        if(file == null)
-            throw new IllegalStateException();
-
+    public static void save() {
+        if (file == null) {
+            throw new IllegalStateException("No MDM configuration was previously loaded.");
+        }
         FileOutputStream out = null;
         try {
             out = new FileOutputStream(file);
             CONFIGURATION.store(out, "MDM configuration file"); //$NON-NLS-1$
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
-        }
-        finally {
+        } finally {
             if (out != null) {
                 try {
                     out.close();
                 } catch (Exception e2) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Error occurred during close() operation during configuration save.", e2);
+                    }
                 }
             }
         }
     }
 
-    public static EDBType getDBType(){
-        Object dbtype=getConfiguration().get("xmldb.type");
-        if(dbtype!=null && dbtype.toString().equals(EDBType.ORACLE.getName())){
+    public static EDBType getDBType() {
+        Object dbType = getConfiguration().get("xmldb.type"); //$NON-NLS-1$
+        if (dbType != null && dbType.toString().equals(EDBType.ORACLE.getName())) {
             return EDBType.ORACLE;
         }
-        if(dbtype!=null && dbtype.toString().equals(EDBType.BERKELEY.getName())){
+        if (dbType != null && dbType.toString().equals(EDBType.BERKELEY.getName())) {
             return EDBType.BERKELEY;
-        }   
-        if(dbtype!=null && dbtype.toString().equals(EDBType.QIZX.getName())){
+        }
+        if (dbType != null && dbType.toString().equals(EDBType.QIZX.getName())) {
             return EDBType.QIZX;
-        } 
+        }
         return EDBType.EXIST;
     }
+
     public static boolean isExistDb() {
-        Object dbtype=getConfiguration().get("xmldb.type");
-        if(dbtype!=null && !dbtype.toString().equals(EDBType.EXIST.getName())) {
-            return false;
-        }
-        return true;
-    }   
-    
+        Object dbType = getConfiguration().get("xmldb.type"); //$NON-NLS-1$
+        return !(dbType != null && !dbType.toString().equals(EDBType.EXIST.getName()));
+    }
+
     public static String getHttpPort() {
         String port = getConfiguration().getProperty("http.server.port"); //$NON-NLS-1$
         port = port == null ? "8180" : port; //$NON-NLS-1$
@@ -153,9 +158,9 @@ public final class MDMConfiguration {
     }
 
     public static String getAdminPassword() {
-        String passwd = getConfiguration().getProperty("admin.password"); //$NON-NLS-1$
-        passwd = passwd == null ? "talend" : passwd; //$NON-NLS-1$
-        return passwd;
+        String password = getConfiguration().getProperty("admin.password"); //$NON-NLS-1$
+        password = password == null ? "talend" : password; //$NON-NLS-1$
+        return password;
     }
 
     public static String getAdminUser() {
@@ -163,7 +168,7 @@ public final class MDMConfiguration {
         user = user == null ? "admin" : user; //$NON-NLS-1$
         return user;
     }
-    
+
     public static int getAutoEntityFindThreshold() {
         String value = getConfiguration().getProperty("autoentityfind.item.max"); //$NON-NLS-1$
         try {
@@ -172,12 +177,9 @@ public final class MDMConfiguration {
             return Integer.MAX_VALUE;
         }
     }
-    
+
     public static boolean isSqlDataBase() {
         String xmlServerClass = getConfiguration().getProperty("xmlserver.class"); //$NON-NLS-1$
-        if ("com.amalto.core.storage.DispatchWrapper".equals(xmlServerClass)) { //$NON-NLS-1$
-            return true;
-        }
-        return false;
+        return "com.amalto.core.storage.DispatchWrapper".equals(xmlServerClass); //$NON-NLS-1$
     }
 }
