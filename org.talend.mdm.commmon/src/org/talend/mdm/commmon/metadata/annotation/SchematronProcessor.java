3162415f87d57686d52716defee345c730008548
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-19 11:08:17 +0000

    Handles more encoding styles for schematron processing.
    git-svn-id: http://talendforge.org/svn/tom/trunk@99819 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.commmon/src/org/talend/mdm/commmon/metadata/annotation/SchematronProcessor.java
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/metadata/annotation/SchematronProcessor.java
@@ -11,6 +11,7 @@
 
 package org.talend.mdm.commmon.metadata.annotation;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.xsd.XSDAnnotation;
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
@@ -18,6 +19,7 @@ import org.talend.mdm.commmon.metadata.MetadataRepository;
 import org.w3c.dom.Element;
 
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
@@ -35,15 +37,25 @@ public class SchematronProcessor implements XmlSchemaAnnotationProcessor {
             EList<Element> annotations = annotation.getApplicationInformation();
             for (Element appInfo : annotations) {
                 if ("X_Schematron".equals(appInfo.getAttribute("source"))) { //$NON-NLS-1$ //$NON-NLS-2$
+                    StringWriter sw = new StringWriter();
+                    Transformer transformer = null;
+                    try {
+                        transformer = transformerFactory.newTransformer();
+                    } catch (TransformerConfigurationException e) {
+                        throw new RuntimeException("Could not create transformer instance.", e);
+                    }
+                    transformer.setOutputProperty("omit-xml-declaration", "yes"); //$NON-NLS-1$ //$NON-NLS-2$
                     try {
                         // TODO This is not really efficient but doing it nicely would require to rewrite a StringEscapeUtils.unescapeXml()
-                        StringWriter sw = new StringWriter();
-                        Transformer transformer = transformerFactory.newTransformer();
-                        transformer.setOutputProperty("omit-xml-declaration", "yes"); //$NON-NLS-1$ //$NON-NLS-2$
                         transformer.transform(new StreamSource(new StringReader(appInfo.getTextContent())), new StreamResult(sw));
                         state.setSchematron("<schema>" + sw.toString() + "</schema>"); //$NON-NLS-1$ //$NON-NLS-2$
                     } catch (TransformerException e) {
-                        throw new RuntimeException(e);
+                        try {
+                            transformer.transform(new StreamSource(new StringReader(StringEscapeUtils.unescapeXml(appInfo.getTextContent()))), new StreamResult(sw));
+                            state.setSchematron("<schema>" + sw.toString() + "</schema>"); //$NON-NLS-1$ //$NON-NLS-2$
+                        } catch (TransformerException e1) {
+                            throw new RuntimeException(e1);
+                        }
                     }
                 }
             }
