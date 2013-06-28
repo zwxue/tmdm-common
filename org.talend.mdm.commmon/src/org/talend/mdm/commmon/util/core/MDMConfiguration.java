1d25e0b22e36884f267f4445d20ece97e0d80f86
chliu <chliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-06-28 01:57:18 +0000

    TMDM-5590 : Correct NLS flags and revert MDMConfiguration
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_2@104611 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/MDMConfiguration.java
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/MDMConfiguration.java
@@ -175,7 +175,9 @@ public final class MDMConfiguration {
     
     public static boolean isSqlDataBase() {
         String xmlServerClass = getConfiguration().getProperty("xmlserver.class"); //$NON-NLS-1$
-        return "com.amalto.core.storage.DispatchWrapper".equals(xmlServerClass)  //$NON-NLS-1$
-                || "com.amalto.core.storage.SQLWrapper".equals(xmlServerClass); //$NON-NLS-1$
+        if ("com.amalto.core.storage.DispatchWrapper".equals(xmlServerClass)) { //$NON-NLS-1$
+            return true;
+        }
+        return false;
     }
 }
