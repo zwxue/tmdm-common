4dbcfc1c5cea2de619c3b8a3f3610c1a80d2b37f
cgpiao <cgpiao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-25 02:28:31 +0000

    merge r105773 from trunk to branch 5.3 TMDM-6096:if pkInfos's containing type is ContainedComplexTypeMetadata,search it's parent container utill that's ComplexTypeMetadata
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_3@105774 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.commmon/src/org/talend/mdm/commmon/metadata/validation/PrimaryKeyInfoValidationRule.java
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/metadata/validation/PrimaryKeyInfoValidationRule.java
@@ -1,23 +1,27 @@
 /*
  * Copyright (C) 2006-2013 Talend Inc. - www.talend.com
- *
+ * 
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- *
- * You should have received a copy of the agreement
- * along with this program; if not, write to Talend SA
- * 9 rue Pages 92150 Suresnes, France
+ * 
+ * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
+ * 92150 Suresnes, France
  */
 
 package org.talend.mdm.commmon.metadata.validation;
 
-import org.talend.mdm.commmon.metadata.*;
-import org.w3c.dom.Element;
+import java.util.List;
 
 import javax.xml.XMLConstants;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+
+import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
+import org.talend.mdm.commmon.metadata.ContainedComplexTypeMetadata;
+import org.talend.mdm.commmon.metadata.FieldMetadata;
+import org.talend.mdm.commmon.metadata.MetadataRepository;
+import org.talend.mdm.commmon.metadata.TypeMetadata;
+import org.talend.mdm.commmon.metadata.ValidationError;
+import org.talend.mdm.commmon.metadata.ValidationHandler;
+import org.w3c.dom.Element;
 
 class PrimaryKeyInfoValidationRule implements ValidationRule {
 
@@ -33,12 +37,15 @@ class PrimaryKeyInfoValidationRule implements ValidationRule {
         List<FieldMetadata> primaryKeyInfo = type.getPrimaryKeyInfo();
         for (FieldMetadata pkInfo : primaryKeyInfo) {
             // PK Info must be defined in the entity (can't reference other entity field).
-            if (!type.equals(pkInfo.getContainingType())) {
-                handler.error(type,
-                        "Primary key info must refer a field of the same entity.",
-                        pkInfo.<Element>getData(MetadataRepository.XSD_DOM_ELEMENT),
-                        pkInfo.<Integer>getData(MetadataRepository.XSD_LINE_NUMBER),
-                        pkInfo.<Integer>getData(MetadataRepository.XSD_COLUMN_NUMBER),
+            ComplexTypeMetadata ctm = pkInfo.getContainingType();
+            while (ctm instanceof ContainedComplexTypeMetadata) {
+                ctm = ((ContainedComplexTypeMetadata) ctm).getContainerType();
+            }
+            if (!type.equals(ctm)) {
+                handler.error(type, "Primary key info must refer a field of the same entity.",
+                        pkInfo.<Element> getData(MetadataRepository.XSD_DOM_ELEMENT),
+                        pkInfo.<Integer> getData(MetadataRepository.XSD_LINE_NUMBER),
+                        pkInfo.<Integer> getData(MetadataRepository.XSD_COLUMN_NUMBER),
                         ValidationError.PRIMARY_KEY_INFO_NOT_IN_ENTITY);
                 success &= false;
                 continue;
@@ -47,21 +54,19 @@ class PrimaryKeyInfoValidationRule implements ValidationRule {
             ValidationFactory.getRule(pkInfo).perform(handler);
             // No need to check isMany() if field definition is already wrong.
             if (pkInfo.isMany()) {
-                handler.error(type,
-                        "Primary key info element cannot be a repeatable element.",
-                        pkInfo.<Element>getData(MetadataRepository.XSD_DOM_ELEMENT),
-                        pkInfo.<Integer>getData(MetadataRepository.XSD_LINE_NUMBER),
-                        pkInfo.<Integer>getData(MetadataRepository.XSD_COLUMN_NUMBER),
+                handler.error(type, "Primary key info element cannot be a repeatable element.",
+                        pkInfo.<Element> getData(MetadataRepository.XSD_DOM_ELEMENT),
+                        pkInfo.<Integer> getData(MetadataRepository.XSD_LINE_NUMBER),
+                        pkInfo.<Integer> getData(MetadataRepository.XSD_COLUMN_NUMBER),
                         ValidationError.PRIMARY_KEY_INFO_CANNOT_BE_REPEATABLE);
                 success &= false;
                 continue;
             }
             if (!isPrimitiveTypeField(pkInfo)) {
-                handler.warning(type,
-                        "Primary key info should refer to a field with a primitive XSD type.",
-                        pkInfo.<Element>getData(MetadataRepository.XSD_DOM_ELEMENT),
-                        pkInfo.<Integer>getData(MetadataRepository.XSD_LINE_NUMBER),
-                        pkInfo.<Integer>getData(MetadataRepository.XSD_COLUMN_NUMBER),
+                handler.warning(type, "Primary key info should refer to a field with a primitive XSD type.",
+                        pkInfo.<Element> getData(MetadataRepository.XSD_DOM_ELEMENT),
+                        pkInfo.<Integer> getData(MetadataRepository.XSD_LINE_NUMBER),
+                        pkInfo.<Integer> getData(MetadataRepository.XSD_COLUMN_NUMBER),
                         ValidationError.PRIMARY_KEY_INFO_TYPE_NOT_PRIMITIVE);
             }
         }
@@ -73,8 +78,7 @@ class PrimaryKeyInfoValidationRule implements ValidationRule {
         if (!XMLConstants.W3C_XML_SCHEMA_NS_URI.equals(currentType.getNamespace())) {
             while (!currentType.getSuperTypes().isEmpty()) {
                 TypeMetadata superType = currentType.getSuperTypes().iterator().next();
-                if (XMLConstants.W3C_XML_SCHEMA_NS_URI.equals(superType.getNamespace())
-                        && ("anyType".equals(superType.getName()) //$NON-NLS-1$
+                if (XMLConstants.W3C_XML_SCHEMA_NS_URI.equals(superType.getNamespace()) && ("anyType".equals(superType.getName()) //$NON-NLS-1$
                         || "anySimpleType".equals(superType.getName()))) { //$NON-NLS-1$
                     break;
                 }
