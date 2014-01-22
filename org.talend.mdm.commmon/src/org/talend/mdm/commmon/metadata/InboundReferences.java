9210cb1b4b77f6b7c1c0b96defb4b33d20661823
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-22 11:43:43 +0000

    TMDM-6706: Move MetadataUtils to org.talend.mdm.commmon module // https://jira.talendforge.org/browse/TMDM-6706 All Storage-related utils moved to com.amalto.core.storage.StorageMetadataUtils.
    git-svn-id: http://talendforge.org/svn/tom/trunk@113599 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/metadata/InboundReferences.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
+
+package org.talend.mdm.commmon.metadata;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Returns the field that references the concept <code>typeName</code>. References are returned as a {@link Set} of
+ * {@link ReferenceFieldMetadata}.
+ */
+public class InboundReferences extends DefaultMetadataVisitor<Set<ReferenceFieldMetadata>> {
+
+    // Internal: for optimization purpose prevents checking a type more than once.
+    private final Set<TypeMetadata> checkedTypes = new HashSet<TypeMetadata>();
+
+    // Foreign key fields list to be returned at end of visit.
+    private final Set<ReferenceFieldMetadata> fieldToCheck = new HashSet<ReferenceFieldMetadata>();
+
+    private final TypeMetadata type;
+
+    /**
+     * This {@link MetadataVisitor} returns foreign key fields that points to <code>type</code>.
+     * @param type A type.
+     */
+    public InboundReferences(TypeMetadata type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Type argument cannot be null"); //$NON-NLS-1$
+        }
+        this.type = type;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(ComplexTypeMetadata metadata) {
+        if (!checkedTypes.contains(metadata)) {
+            checkedTypes.add(metadata);
+            return super.visit(metadata);
+        }
+        return fieldToCheck;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(ReferenceFieldMetadata metadata) {
+        if (type.isAssignableFrom(metadata.getReferencedType())) {
+            fieldToCheck.add(metadata);
+        }
+        for (ComplexTypeMetadata subType : metadata.getReferencedType().getSubTypes()) {
+            subType.accept(this);
+        }
+        super.visit(metadata);
+        return fieldToCheck;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(SimpleTypeMetadata typeMetadata) {
+        return fieldToCheck;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(MetadataRepository repository) {
+        Collection<TypeMetadata> types = repository.getTypes();
+        for (TypeMetadata type : types) {
+            type.accept(this);
+        }
+        return fieldToCheck;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(FieldMetadata metadata) {
+        super.visit(metadata);
+        return fieldToCheck;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(SimpleTypeFieldMetadata metadata) {
+        super.visit(metadata);
+        return fieldToCheck;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(EnumerationFieldMetadata metadata) {
+        super.visit(metadata);
+        return fieldToCheck;
+    }
+
+    @Override
+    public Set<ReferenceFieldMetadata> visit(ContainedTypeFieldMetadata metadata) {
+        super.visit(metadata);
+        for (ComplexTypeMetadata subType : metadata.getContainedType().getSubTypes()) {
+            subType.accept(this);
+        }
+        return fieldToCheck;
+    }
+}
---
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/metadata/MetadataUtils.java
@@ -0,0 +1,385 @@
+/*
+ * Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
+
+package org.talend.mdm.commmon.metadata;
+
+import javax.xml.XMLConstants;
+import java.util.*;
+
+public class MetadataUtils {
+
+    private MetadataUtils() {
+    }
+
+    private static final double ENTITY_RANK_ADJUST = 0.9;
+
+    /**
+     * <p>
+     * Computes "entity rank": entity rank score is based on a modified version of Google's Page Rank algorithm (it's
+     * the inverse operation of Page Rank).
+     * </p>
+     * <p>
+     * Entity rank is computed with this algorithm:
+     * ER(E) = N + d (ER(E1)/C(E1) + ... + ER(En)/C(En))
+     * where:
+     * <ul>
+     * <li>ER(E) is the entity rank of E.</li>
+     * <li>N the number of entities in <code>repository</code></li>
+     * <li>d an adjustment factor (between 0 and 1)</li>
+     * <li>ER(Ei) is the entity rank for entity Ei that E references via a reference field</li>
+     * <li>C(Ei) the number of entities in <code>repository</code> that reference Ei in the repository.</li>
+     * </ul>
+     * </p>
+     * <p>
+     * Code is expected to run in linear time (O(n+p) where n is the number of entities and p the number of references).
+     * Used memory is O(n^2) (due to a dependency ordering).
+     * </p>
+     *
+     * @param repository A {@link MetadataRepository} instance that contains entity types.
+     * @return A {@link Map} that maps a entity to its entity rank value.
+     */
+    public static Map<ComplexTypeMetadata, Long> computeEntityRank(MetadataRepository repository) {
+        List<ComplexTypeMetadata> sortedTypes = sortTypes(repository);
+        int totalNumber = sortedTypes.size();
+
+        Map<ComplexTypeMetadata, Long> entityRank = new HashMap<ComplexTypeMetadata, Long>();
+        for (ComplexTypeMetadata currentType : sortedTypes) {
+            if (currentType.isInstantiable()) {
+                double rank = totalNumber;
+                for (FieldMetadata currentField : currentType.getFields()) {
+                    if (currentField instanceof ReferenceFieldMetadata) {
+                        ComplexTypeMetadata referencedType = ((ReferenceFieldMetadata) currentField).getReferencedType();
+                        if (referencedType != currentType) {
+                            Long referencedEntityRank = entityRank.get(referencedType);
+                            if (referencedEntityRank != null) {
+                                double inboundReferencesCount = getInboundReferencesCount(repository, referencedType);
+                                rank += ENTITY_RANK_ADJUST * (referencedEntityRank / inboundReferencesCount);
+                            }
+                        }
+                    }
+                }
+                entityRank.put(currentType, Math.round(rank));
+            }
+        }
+        return entityRank;
+    }
+
+    private static double getInboundReferencesCount(MetadataRepository repository, ComplexTypeMetadata referencedType) {
+        return repository.accept(new InboundReferences(referencedType)).size();
+    }
+
+    /**
+     * Returns the top level type for <code>type</code> parameter: this method returns the type before <i>anyType</i>
+     * in type hierarchy. This does not apply to types declared in {@link XMLConstants#W3C_XML_SCHEMA_NS_URI}.
+     * <ul>
+     * <li>In an MDM entity B inherits from A, getSuperConcreteType(B) returns A.</li>
+     * <li>If a simple type LimitedString extends xsd:string, getSuperConcreteType(LimitedString) returns xsd:string.</li>
+     * <li>getSuperConcreteType(xsd:long) returns xsd:long (even if xsd:long extends xsd:decimal).</li>
+     * <li>If the type does not have any super type, this method returns the <code>type</code> parameter.</li>
+     * </ul>
+     *
+     * @param type A non null type that may have super types.
+     * @return The higher type in inheritance tree before <i>anyType</i>.
+     */
+    public static TypeMetadata getSuperConcreteType(TypeMetadata type) {
+        if (type == null) {
+            return null;
+        }
+        // Move up the inheritance tree to find the "most generic" type (used when simple types inherits from XSD types,
+        // in this case, the XSD type is interesting, not the custom one).
+        while (!XMLConstants.W3C_XML_SCHEMA_NS_URI.equals(type.getNamespace()) && !type.getSuperTypes().isEmpty()) {
+            type = type.getSuperTypes().iterator().next();
+        }
+        return type;
+    }
+
+    /**
+     * @param javaClassName A java class name.
+     * @return The XSD type that can be used to store a value typed as <code>javaClassName</code>.
+     * @throws UnsupportedOperationException If there's no known mapping from this java class to a XSD primitive type.
+     */
+    public static String getType(String javaClassName) {
+        if ("java.lang.String".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.STRING;
+        } else if ("java.lang.Integer".equals(javaClassName) //$NON-NLS-1$
+                || "java.math.BigInteger".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.INT;
+        } else if ("java.lang.Boolean".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.BOOLEAN;
+        } else if ("java.math.BigDecimal".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.DECIMAL;
+        } else if ("java.sql.Timestamp".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.DATETIME;
+        } else if ("java.lang.Short".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.SHORT;
+        } else if ("java.lang.Long".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.LONG;
+        } else if ("java.lang.Float".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.FLOAT;
+        } else if ("java.lang.Byte".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.BYTE;
+        } else if ("java.lang.Double".equals(javaClassName)) { //$NON-NLS-1$
+            return Types.DOUBLE;
+        } else {
+            throw new UnsupportedOperationException("No support for java class '" + javaClassName + "'");
+        }
+    }
+
+    /**
+     * <p>
+     * Sorts type in inverse order of dependency (topological sort). A dependency to <i>type</i> might be:
+     * <ul>
+     * <li>FK reference to <i>type</i> (sub types of <i>type</i> are all included as a dependency).</li>
+     * <li>Use of <i>type</i> as a super type.</li>
+     * </ul>
+     * This method runs in linear time <i>O(n+p)</i> (<i>n</i> number of types and <i>p</i> number of dependencies
+     * between types). This method uses <i>n²</i> bytes in memory for processing.
+     * </p>
+     * <p>
+     * This method is thread safe.
+     * </p>
+     *
+     * @param repository The repository that contains entity types to sort.
+     * @return A sorted list of {@link ComplexTypeMetadata} types. First type of list is a type that has no dependency on
+     *         any other type of the list.
+     * @throws IllegalArgumentException If repository contains types that creates a cyclic dependency. Error message contains
+     *                                  information on where the cycle is.
+     */
+    public static List<ComplexTypeMetadata> sortTypes(MetadataRepository repository) {
+        ArrayList<ComplexTypeMetadata> types = new ArrayList<ComplexTypeMetadata>(repository.getUserComplexTypes());
+        return _sortTypes(repository, false, types);
+    }
+
+    /**
+     * <p>
+     * Sorts types (usually a sub set of types in a {@link MetadataRepository}) in inverse order of dependency
+     * (topological sort). A dependency to <i>type</i> might be:
+     * <ul>
+     * <li>FK reference to <i>type</i> (sub types of <i>type</i> are all included as a dependency).</li>
+     * <li>Use of <i>type</i> as a super type.</li>
+     * </ul>
+     * This method runs in linear time <i>O(n+p)</i> (<i>n</i> number of types and <i>p</i> number of dependencies
+     * between types). This method uses <i>n²</i> bytes in memory for processing.
+     * </p>
+     * <p>
+     * This method is thread safe.
+     * </p>
+     *
+     * @param repository This is used to display information in case of cycle.
+     * @param types The list of types to be sorted. This list should provide a transitive closure of types (all references
+     *              to other types must be satisfied in this list), if it isn't the unresolved FK will be ignored.
+     * @return A sorted list of {@link ComplexTypeMetadata} types. First type of list is a type that has no dependency on
+     *         any other type of the list.
+     * @throws IllegalArgumentException If repository contains types that creates a cyclic dependency. Error message contains
+     *                                  information on where the cycle is.
+     */
+    public static List<ComplexTypeMetadata> sortTypes(MetadataRepository repository, List<ComplexTypeMetadata> types) {
+        return _sortTypes(repository, true, types);
+    }
+
+    private static List<ComplexTypeMetadata> _sortTypes(MetadataRepository repository,
+                                                        final boolean sortAllTypes,
+                                                        final List<ComplexTypeMetadata> types) {
+        /*
+         * Compute additional data for topological sorting
+         */
+        final int typeNumber = types.size();
+        byte[][] dependencyGraph = new byte[typeNumber][typeNumber];
+        for (final ComplexTypeMetadata type : types) {
+            dependencyGraph[getId(type, types)] = type.accept(new DefaultMetadataVisitor<byte[]>() {
+                Set<TypeMetadata> processedTypes = new HashSet<TypeMetadata>();
+
+                byte[] lineContent = new byte[typeNumber]; // Stores dependencies of current type
+
+                @Override
+                public byte[] visit(ComplexTypeMetadata complexType) {
+                    if (processedTypes.contains(complexType)) {
+                        return lineContent;
+                    } else {
+                        processedTypes.add(complexType);
+                    }
+                    if (sortAllTypes || complexType.isInstantiable()) {
+                        Collection<TypeMetadata> superTypes = complexType.getSuperTypes();
+                        for (TypeMetadata superType : superTypes) {
+                            if (superType instanceof ComplexTypeMetadata) {
+                                lineContent[getId(((ComplexTypeMetadata) superType), types)]++;
+                            }
+                        }
+                        super.visit(complexType);
+                    }
+                    if (complexType.isInstantiable()) {
+                        processedTypes.clear();
+                    }
+                    return lineContent;
+                }
+
+                @Override
+                public byte[] visit(ContainedTypeFieldMetadata containedField) {
+                    ComplexTypeMetadata containedType = containedField.getContainedType();
+                    if (processedTypes.contains(containedType)) {
+                        return lineContent;
+                    } else {
+                        processedTypes.add(containedType);
+                    }
+                    containedType.accept(this);
+                    for (ComplexTypeMetadata subType : containedType.getSubTypes()) {
+                        if (processedTypes.contains(subType)) {
+                            return lineContent;
+                        } else {
+                            processedTypes.add(subType);
+                            subType.accept(this);
+                        }
+                    }
+                    return lineContent;
+                }
+
+                @Override
+                public byte[] visit(ReferenceFieldMetadata referenceField) {
+                    ComplexTypeMetadata referencedType = referenceField.getReferencedType();
+                    if (!type.equals(referencedType) && referenceField.isFKIntegrity()) { // Don't count a dependency to itself as a dependency.
+                        if (sortAllTypes || referencedType.isInstantiable()) {
+                            if (types.contains(referencedType)) {
+                                lineContent[getId(referencedType, types)]++;
+                                // Implicitly include reference to sub types of referenced type.
+                                for (ComplexTypeMetadata subType : referencedType.getSubTypes()) {
+                                    lineContent[getId(subType, types)]++;
+                                }
+                            }
+                        }
+                    }
+                    return lineContent;
+                }
+            });
+        }
+        /*
+        * TOPOLOGICAL SORTING
+        * See "Kahn, A. B. (1962), "Topological sorting of large networks", Communications of the ACM"
+        */
+        List<ComplexTypeMetadata> sortedTypes = new LinkedList<ComplexTypeMetadata>();
+        Set<ComplexTypeMetadata> noIncomingEdges = new HashSet<ComplexTypeMetadata>();
+        int lineNumber = 0;
+        for (byte[] line : dependencyGraph) {
+            if (!hasIncomingEdges(line)) {
+                noIncomingEdges.add(getType(types, lineNumber));
+            }
+            lineNumber++;
+        }
+        while (!noIncomingEdges.isEmpty()) {
+            Iterator<ComplexTypeMetadata> iterator = noIncomingEdges.iterator();
+            ComplexTypeMetadata type = iterator.next();
+            iterator.remove();
+            sortedTypes.add(type);
+            int columnNumber = getId(type, types);
+            for (int i = 0; i < typeNumber; i++) {
+                int edge = dependencyGraph[i][columnNumber];
+                if (edge > 0) {
+                    dependencyGraph[i][columnNumber] -= edge;
+                    if (!hasIncomingEdges(dependencyGraph[i])) {
+                        noIncomingEdges.add(getType(types, i));
+                    }
+                }
+            }
+        }
+        // Check for cycles
+        if (sortedTypes.size() < dependencyGraph.length) {
+            lineNumber = 0;
+            List<List<ComplexTypeMetadata>> cycles = new LinkedList<List<ComplexTypeMetadata>>();
+            // use dependency graph matrix to get cyclic dependencies (if any).
+            for (byte[] line : dependencyGraph) {
+                if (hasIncomingEdges(line)) { // unresolved dependency (means this is a cycle start).
+                    List<ComplexTypeMetadata> dependencyPath = new LinkedList<ComplexTypeMetadata>();
+                    int currentLineNumber = lineNumber;
+                    do {
+                        ComplexTypeMetadata type = getType(types, currentLineNumber);
+                        dependencyPath.add(type);
+                        InboundReferences incomingReferences = new InboundReferences(type);
+                        Set<ReferenceFieldMetadata> incomingFields = repository.accept(incomingReferences);
+                        boolean hasMetDependency = false;
+                        for (ReferenceFieldMetadata incomingField : incomingFields) {
+                            ComplexTypeMetadata containingType = repository.getComplexType(incomingField.getEntityTypeName());
+                            int currentDependency = getId(containingType, types);
+                            if (hasIncomingEdges(dependencyGraph[currentDependency])) {
+                                dependencyGraph[currentLineNumber][currentDependency]--;
+                                currentLineNumber = currentDependency;
+                                hasMetDependency = true;
+                                break;
+                            }
+                        }
+                        if (!hasMetDependency) {
+                            break;
+                        }
+                    } while (currentLineNumber != lineNumber);
+                    if (dependencyPath.size() > 1) {
+                        dependencyPath.add(getType(types, lineNumber)); // Include cycle start to get a better exception message.
+                        cycles.add(dependencyPath);
+                    }
+                }
+                lineNumber++;
+            }
+            if (!cycles.isEmpty()) { // Found cycle(s): report it/them as exception
+                StringBuilder cyclesAsString = new StringBuilder();
+                int i = 1;
+                Iterator<List<ComplexTypeMetadata>> cyclesIterator = cycles.iterator();
+                while (cyclesIterator.hasNext()) {
+                    cyclesAsString.append(i++).append(") "); //$NON-NLS-1$
+                    Iterator<ComplexTypeMetadata> dependencyPathIterator = cyclesIterator.next().iterator();
+                    ComplexTypeMetadata previous = null;
+                    while (dependencyPathIterator.hasNext()) {
+                        ComplexTypeMetadata currentType = dependencyPathIterator.next();
+                        cyclesAsString.append(currentType.getName());
+                        if (dependencyPathIterator.hasNext()) {
+                            cyclesAsString.append(" -> "); //$NON-NLS-1$
+                        } else if (previous != null) {
+                            Set<ReferenceFieldMetadata> inboundReferences = repository.accept(new InboundReferences(currentType));
+                            cyclesAsString.append(" ( possible fields: ");
+                            for (ReferenceFieldMetadata inboundReference : inboundReferences) {
+                                String xPath = inboundReference.getPath();
+                                cyclesAsString.append(xPath).append(' ');
+                            }
+                            cyclesAsString.append(')');
+                        }
+                        previous = currentType;
+                    }
+                    if (cyclesIterator.hasNext()) {
+                        cyclesAsString.append('\n');
+                    }
+                }
+                throw new IllegalArgumentException("Data model has circular dependencies:\n" + cyclesAsString);
+            }
+        }
+        return sortedTypes;
+    }
+
+    // internal method for sortTypes
+    private static ComplexTypeMetadata getType(List<ComplexTypeMetadata> types, int lineNumber) {
+        return types.get(lineNumber);
+    }
+
+    // internal method for sortTypes
+    private static boolean hasIncomingEdges(byte[] line) {
+        for (byte column : line) {
+            if (column > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // internal method for sortTypes
+    private static int getId(ComplexTypeMetadata type, List<ComplexTypeMetadata> types) {
+        if (!types.contains(type)) {
+            types.add(type);
+        }
+        return types.indexOf(type);
+    }
+
+}
+
