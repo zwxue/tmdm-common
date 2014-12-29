463c33c8981e791502498f9a12f8d7675177b26d
fhuaulme <fhuaulme@talend.com>
2014-12-29 11:06:20 +0100

    TMDM-8055: "Tomcat as the MDM server"
    Continue revision id and universe removal.
    (https://jira.talendforge.org/browse/TMDM-8055).

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/webapp/XSystemObjects.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/webapp/XSystemObjects.java
@@ -6,36 +6,27 @@ import java.util.Map;
 import org.talend.mdm.commmon.util.core.ICoreConstants;
 
 public enum XSystemObjects {
-    // Data Clusters
-    DC_JCAADAPTERS(XObjectType.DATA_CLUSTER, "JCAAdapters", true), //$NON-NLS-1$
-    DC_INBOX(XObjectType.DATA_CLUSTER, "Inbox", false), //$NON-NLS-1$
     DC_CONF(XObjectType.DATA_CLUSTER, "CONF", false), //$NON-NLS-1$
     DC_PROVISIONING(XObjectType.DATA_CLUSTER, "PROVISIONING", true), //$NON-NLS-1$
     DC_UPDATE_PREPORT(XObjectType.DATA_CLUSTER, "UpdateReport", false), //$NON-NLS-1$
-    DC_XTENTIS_COMMON_REPORTING(XObjectType.DATA_CLUSTER, "Reporting", false), //$NON-NLS-1$
     DC_MDMITEMSTRASH(XObjectType.DATA_CLUSTER, "MDMItemsTrash", false), //$NON-NLS-1$
     DC_SEARCHTEMPLATE(XObjectType.DATA_CLUSTER, "SearchTemplate", true), //$NON-NLS-1$
     DC_CROSSREFERENCING(XObjectType.DATA_CLUSTER, ICoreConstants.CrossReferencing_datacluster, true),
-    // Revision
-    DC_REVISION(XObjectType.DATA_CLUSTER, "Revision", true), //$NON-NLS-1$
+    //$NON-NLS-1$
     DC_MDMItemImages(XObjectType.DATA_CLUSTER, "MDMItemImages", false), //$NON-NLS-1$
     DC_MDMMigration(XObjectType.DATA_CLUSTER, "MDMMigration", false), //$NON-NLS-1$
     // Role
     ROLE_DEFAULT_ADMIN(XObjectType.ROLE, ICoreConstants.SYSTEM_ADMIN_ROLE, false),
     ROLE_DEFAULT_USER(XObjectType.ROLE, ICoreConstants.SYSTEM_INTERACTIVE_ROLE, false),
     ROLE_DEFAULT_WEB(XObjectType.ROLE, ICoreConstants.SYSTEM_WEB_ROLE, false),
-    ROLE_DEFAULT_VIEWER(XObjectType.ROLE, ICoreConstants.SYSTEM_VIEW_ROLE, true), // readonly for everything, can't be
-                                                                                  // modified
+    // readonly for everything, can't be modified
+    ROLE_DEFAULT_VIEWER(XObjectType.ROLE, ICoreConstants.SYSTEM_VIEW_ROLE, true),
     // Data Models
     DM_CONF(XObjectType.DATA_MODEL, "CONF", false), //$NON-NLS-1$
     DM_PROVISIONING(XObjectType.DATA_MODEL, "PROVISIONING", true), //$NON-NLS-1$
     DM_UPDATEREPORT(XObjectType.DATA_MODEL, "UpdateReport", false), //$NON-NLS-1$
-    DM_XTENTIS_COMMON_REPORTING(XObjectType.DATA_MODEL, "Reporting", false), //$NON-NLS-1$
     DM_SEARCHTEMPLATE(XObjectType.DATA_MODEL, "SearchTemplate", true), //$NON-NLS-1$
     Dm_CROSSREFERENCING(XObjectType.DATA_MODEL, ICoreConstants.CrossReferencing_datamodel, true),
-    // Menus
-    M_BROWSE_ITEMS2(XObjectType.MENU, "Browse items2", false), //$NON-NLS-1$
-    // GXT refact menu
     M_BrowseRecords(XObjectType.MENU, "BrowseRecords", false), //$NON-NLS-1$
     M_Hierarchy(XObjectType.MENU, "Hierarchy", true), //$NON-NLS-1$
     M_WelcomePortal(XObjectType.MENU, "WelcomePortal", false), //$NON-NLS-1$
@@ -48,15 +39,9 @@ public enum XSystemObjects {
     M_MANAGER_USERS(XObjectType.MENU, "Manage users", true), //$NON-NLS-1$
     M_LICENSE(XObjectType.MENU, "License", true), //$NON-NLS-1$
     M_REPORTING(XObjectType.MENU, "Reporting", false), //$NON-NLS-1$
-    M_SYNCHRONIZATIONACTION(XObjectType.MENU, "SynchronizationAction", true), //$NON-NLS-1$
-    M_SYNCHRONIZATIONPLAN(XObjectType.MENU, "SynchronizationItem", true), //$NON-NLS-1$
     M_UPDATE_REPORT(XObjectType.MENU, "UpdateReport", false), //$NON-NLS-1$
-    M_SERVICE_SCHEDULE(XObjectType.MENU, "Service Schedule", true), //$NON-NLS-1$
-    M_LOGGING(XObjectType.MENU, "logging", false), //$NON-NLS-1$
     M_ITEMSTRASH(XObjectType.MENU, "ItemsTrash", false), //$NON-NLS-1$
-    M_SMTP(XObjectType.MENU, "smtp", false), //$NON-NLS-1$
     M_HIERARCHICAL_VIEW_DERIVED(XObjectType.MENU, "Derived Hierarchy", true), //$NON-NLS-1$
-    M_UNIVERSEMANAGER(XObjectType.MENU, "Universe Manager", true), //$NON-NLS-1$
     M_CROSSREFERENCING(XObjectType.MENU, "Cross Referencing", true), //$NON-NLS-1$
     M_WORKFLOWTASKS(XObjectType.MENU, "WorkflowTasks", true), //$NON-NLS-1$
     M_DATASTEWARDSHIP(XObjectType.MENU, "Datastewardship", true), //$NON-NLS-1$
