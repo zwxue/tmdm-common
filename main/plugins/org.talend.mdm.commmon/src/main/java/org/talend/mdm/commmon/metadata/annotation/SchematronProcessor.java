71bbb6e3542c68f89e1d40d731c87991594f55e9
Li Yunjiao <yjli@talend.com>
2016-11-25 16:44:15 +0800

    TMDM-10164 Move 'XmlUtil' class to 'org.talend.mdm.common.core' project. (#11)
    - Avoid introducing dependency for Studio build.

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/annotation/SchematronProcessor.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/annotation/SchematronProcessor.java
@@ -16,6 +16,7 @@ import java.io.StringWriter;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
@@ -24,10 +25,11 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.xsd.XSDAnnotation;
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.MetadataRepository;
-import org.talend.mdm.commmon.util.core.XmlUtil;
 import org.w3c.dom.Element;
 
 public class SchematronProcessor implements XmlSchemaAnnotationProcessor {
+
+    private static final TransformerFactory transformerFactory = TransformerFactory.newInstance();
 
     @Override
     public void process(MetadataRepository repository, ComplexTypeMetadata type, XSDAnnotation annotation,
@@ -39,10 +41,11 @@ public class SchematronProcessor implements XmlSchemaAnnotationProcessor {
                     StringWriter sw = new StringWriter();
                     Transformer transformer;
                     try {
-                        transformer = XmlUtil.generateTransformer(true);
+                        transformer = transformerFactory.newTransformer();
                     } catch (TransformerConfigurationException e) {
                         throw new RuntimeException("Could not create transformer instance.", e);
                     }
+                    transformer.setOutputProperty("omit-xml-declaration", "yes"); //$NON-NLS-1$ //$NON-NLS-2$
                     try {
                         // TODO This is not really efficient but doing it nicely would require to rewrite a
                         // StringEscapeUtils.unescapeXml()
