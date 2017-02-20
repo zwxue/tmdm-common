8ee8ac77aa1d8bf977eef3d86820ec8da99a3f7e
zfkang-talend <zfkang@talend.com>
2017-02-20 11:02:06 +0800

    TMDM-10531: [Impact Analysis] Move a simple field from mandatory to optional (#18)

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/compare/HibernateStorageImpactAnalyzer.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/compare/HibernateStorageImpactAnalyzer.java
@@ -124,14 +124,18 @@ public class HibernateStorageImpactAnalyzer implements ImpactAnalyzer {
                     // Key creation might have high impact (in case of duplicated values).
                     impactSort.get(Impact.HIGH).add(modifyAction);
                 } else if (previous.isMandatory() != current.isMandatory()) {
-                    // Won't be able to change constraint
-                    String defaultValueRule = ((FieldMetadata) current).getData(MetadataRepository.DEFAULT_VALUE_RULE);
-                    if (!modifyAction.isHasNullValue()) {
+                    if (!previous.isMandatory() && current.isMandatory()) {
+                        // Won't be able to change constraint
+                        String defaultValueRule = ((FieldMetadata) current).getData(MetadataRepository.DEFAULT_VALUE_RULE);
+                        if (!modifyAction.isHasNullValue()) {
+                            impactSort.get(Impact.LOW).add(modifyAction);
+                        } else if (modifyAction.isHasNullValue() && StringUtils.isBlank(defaultValueRule)) {
+                            impactSort.get(Impact.HIGH).add(modifyAction);
+                        } else if (modifyAction.isHasNullValue() && StringUtils.isNotBlank(defaultValueRule)) {
+                            impactSort.get(Impact.MEDIUM).add(modifyAction);
+                        }
+                    } else if(previous.isMandatory() && !current.isMandatory()){
                         impactSort.get(Impact.LOW).add(modifyAction);
-                    } else if (modifyAction.isHasNullValue() && StringUtils.isBlank(defaultValueRule)) {
-                        impactSort.get(Impact.HIGH).add(modifyAction);
-                    } else if (modifyAction.isHasNullValue() && StringUtils.isNotBlank(defaultValueRule)) {
-                        impactSort.get(Impact.MEDIUM).add(modifyAction);
                     }
                 }
                 
