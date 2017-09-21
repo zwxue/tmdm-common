2257efaa743d1235c45589bcdd35c816c59115c4
sbliu <sbliu@talend.com>
2017-09-21 18:04:48 +0800

    TMDM-11332 Treat missing LicenseManager menu dependency as a warning.

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/webapp/XSystemObjects.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/webapp/XSystemObjects.java
@@ -47,6 +47,7 @@ public enum XSystemObjects {
     M_DATASTEWARDSHIP(XObjectType.MENU, "Datastewardship", true), //$NON-NLS-1$
     M_STAGINGAREA(XObjectType.MENU, "StagingArea", false), //$NON-NLS-1$
     M_BROWSE_RECORDS_STAGING(XObjectType.MENU, "BrowseRecordsInStaging", false), //$NON-NLS-1$
+    M_LICENSEMANAGER(XObjectType.MENU, "LicenseManager", true), //$NON-NLS-1$
     M_BONITAWORKFLOWTASKS(XObjectType.MENU, "BonitaWorkflowTasks", true); //$NON-NLS-1$
 
     private String name;
